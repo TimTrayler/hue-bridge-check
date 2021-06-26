612d2de8700ce8d2c245c7d0a4b34ae6383f6069
Tim Trayler <67274738+TimTrayler@users.noreply.github.com>
2021-06-26 21:56:30 +0200

    Using fetch instead of XMLHttpRequest

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
 function check()
 {
-    var x = new XMLHttpRequest();
-
-    x.onreadystatechange = function()
-    {
-        if(x.readyState == x.DONE)
-        {
-            var out = document.getElementById("output");
+    return new Promise(async (resolve, reject) => {
+        const response = await fetch("https://discovery.meethue.com/");
+        const out = document.getElementById("output");
             
-            if(x.status > 199 && x.status < 300)
+        if(response.status > 199 && response.status < 300)
+        {
+            const bridges = await response.json();
+
+            out.innerHTML = "You have " + bridges.length + " bridge " + ((bridges.length > 1 || bridges.length < 1) ? "s" : "") + " in your local network!";
+
+            if(bridges.length > 0)
             {
-                var bridges = JSON.parse(x.responseText);
-                
-                out.innerHTML = "You have " + bridges.length + " bridge " + ((bridges.length > 1 || bridges.length < 1) ? "s" : "") + " in your Wi-Fi!";
+                out.innerText += "\n\n\nBridges:\n";
 
-                if(bridges.length > 0)
+                for(var i = 0; i < bridges.length; i++)
                 {
-                    out.innerText += "\n\n\nBridges:\n";
-                    
-                    for(var i = 0; i < bridges.length; i++)
-                    {
-                        out.innerText += "\n IP-Adress: \"" + bridges[i]["internalipaddress"] + "\", ID: \"" + bridges[i]["id"] + "\"\n"
-                    }
+                    out.innerText += "\n IP-Adress: \"" + bridges[i]["internalipaddress"] + "\", ID: \"" + bridges[i]["id"] + "\"\n"
                 }
-
-            }else {
-                out.innerHTML = "<p error>An error occurred! Make sure you have allowed Requests to "
-                                + "<a href=\"https://discovery.meethue.com/\" target=\"_blank\"><code>discovery.meethue.com</code></a>!</p>";
             }
-        }
-    }
 
-    x.open("GET", "https://discovery.meethue.com/");
-    x.send();
+        }else {
+            out.innerHTML = "<p error>An error occurred! Make sure you have allowed requests to "
+                            + "<a href=\"https://discovery.meethue.com/\" target=\"_blank\"><code>discovery.meethue.com</code></a></p>";
+        }
+    });
 }
